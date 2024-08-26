d4125ff7b9ec7be1767676280fead7acdd718f12
IgnacioPuelles <139293045+IgnacioPuelles@users.noreply.github.com>
2024-08-25 21:39:20 -0400

    login-validaciones

--- myApp/src/app/login/login.page.ts
+++ myApp/src/app/login/login.page.ts
@@ -1,11 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { AlertController } from '@ionic/angular';
+import type { IonInput } from '@ionic/angular';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.page.html',
   styleUrls: ['./login.page.scss'],
 })
+
 export class LoginPage implements OnInit {
 
   login:any={
@@ -17,36 +19,45 @@ export class LoginPage implements OnInit {
   }
 
   ingresar(){
-    console.log(this.login.usuario);
-
     console.log(this.validarLogin(this.login));
   }
 
   validarLogin(model:any){
     if(model.usuario=="" || model.usuario.length > 8 || model.usuario.length < 3){
       this.presentAlert();
-        return "Usuario vacio";
-    }else if(model.password==""){
+    }else if(model.password=="" || model.password.length != 4){
       this.presentAlert();
-      return "Password vacio";
     }
-
-    return "Campos completos";
-
   }
 
 constructor(private alertController: AlertController) {}
 
   async presentAlert() {
     const alert = await this.alertController.create({
-      header: 'A Short Title Is Best',
-      subHeader: 'A Sub Header Is Optional',
-      message: 'A message should be a short, complete sentence.',
-      buttons: ['Action'],
+      header: 'Error al iniciar sesión',
+      message: 'Usuario o contraseña incorrectos',
+      cssClass: 'alertaLogin',
+      buttons: [{text: 'OK',
+      cssClass: 'alert-button'}],
+      
     });
 
     await alert.present();
   }
 
+  inputModel = '';
+
+  @ViewChild('ionInputLog', { static: true }) ionInputLog!: IonInput;
+
+  onInput(ev: Event) {
+    const inputElement = ev.target as HTMLInputElement;
+    const value = inputElement.value;
+
+    const filteredValue = value.replace(/\D+/g, '');
+
+    this.ionInputLog.value = this.inputModel = filteredValue;
+  }
+
 }  
+
 
