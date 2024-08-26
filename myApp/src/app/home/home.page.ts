91806788fb464bc47a3268e12247afc98e97086b
Javier <javie.silvav@duocuc.cl>
2024-08-26 19:25:25 -0400

    cambiasos

--- myApp/src/app/home/home.page.ts
+++ myApp/src/app/home/home.page.ts
@@ -15,9 +15,10 @@ export class HomePage {
 
     this.activeroute.queryParams.subscribe(params => {
       const navigation = this.router.getCurrentNavigation();
-      if (navigation?.extras?.state) {
+      if (navigation && navigation.extras && navigation.extras.state) {
         this.usuario = navigation.extras.state['user'];
       }
+
     });
   }
 
@@ -25,17 +26,18 @@ export class HomePage {
   limpiarFormulario() {
     (document.getElementById('nombre') as HTMLInputElement).value = '';
     (document.getElementById('apellido') as HTMLInputElement).value = '';
-    (document.getElementById('nivelEducacional') as HTMLIonSelectOptionElement).value = '';
+    (document.getElementById('nivelEducacional') as HTMLIonSelectElement).value = '';
     (document.getElementById('fechaNac') as HTMLIonDatetimeElement).value = '';
   }
 
   mostrarDatos() {
     const nombre = (document.getElementById('nombre') as HTMLInputElement).value;
     const apellido = (document.getElementById('apellido') as HTMLInputElement).value;
-    const nivelEducacional = (document.getElementById('nivelEducacional') as HTMLIonSelectOptionElement).value;
+    const nivelEducacional = (document.getElementById('nivelEducacional') as HTMLIonSelectElement).value;
     const fechaNac = (document.getElementById('fechaNac') as HTMLIonDatetimeElement).value;
-
+  
     alert(`Nombre: ${nombre}\nApellido: ${apellido}\nNivel Educacional: ${nivelEducacional}\nFecha de nacimiento: ${fechaNac}`);
   }
+  
 }
 
