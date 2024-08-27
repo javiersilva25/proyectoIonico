0fb95ad47ac8066d8be1ccff49be1bff539c0ca1
Javier <javie.silvav@duocuc.cl>
2024-08-27 08:39:32 -0400

    cambios aún mas adicionales

--- myApp/src/app/app.module.ts
+++ myApp/src/app/app.module.ts
@@ -6,13 +6,14 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 @NgModule({
   declarations: [AppComponent],
   imports: [BrowserModule, 
     IonicModule.forRoot({innerHTMLTemplatesEnabled: true}), 
     AppRoutingModule],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
+  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }, provideAnimationsAsync()],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
