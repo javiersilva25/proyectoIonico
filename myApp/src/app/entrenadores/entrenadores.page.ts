cdc8556b19c71315a8672932d36afe0e830b7dea
Javier <javie.silvav@duocuc.cl>
2024-09-03 18:18:21 -0400

    perdon
    murió la app

--- myApp/src/app/entrenadores/entrenadores.page.ts
+++ myApp/src/app/entrenadores/entrenadores.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { MenuController } from '@ionic/angular';
 
 @Component({
   selector: 'app-entrenadores',
@@ -7,9 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EntrenadoresPage implements OnInit {
 
-  constructor() { }
+  constructor(private menu: MenuController) { }
 
   ngOnInit() {
+  }
+
+  closeMenu() {
+    this.menu.close();
   }
 
 }
--- myApp/src/app/entrenamientos/entrenamientos.page.ts
+++ myApp/src/app/entrenamientos/entrenamientos.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { MenuController } from '@ionic/angular';
 
 @Component({
   selector: 'app-entrenamientos',
@@ -7,9 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EntrenamientosPage implements OnInit {
 
-  constructor() { }
+  constructor(private menu: MenuController) { }
 
   ngOnInit() {
+  }
+
+  closeMenu() {
+    this.menu.close();
   }
 
 }
